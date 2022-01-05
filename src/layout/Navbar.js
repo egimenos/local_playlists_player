3bdcc649ff1159d7994ed91b14fee29b0702aa51
EgimenoS <ergisan@gmail.com>
2022-01-05 19:21:33 +0100

    feat: added navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,3 +1,14 @@
-const Navbar = () => {};
+import { Box, Flex } from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+
+const Navbar = () => {
+	return (
+		<Box boxShadow='lg' bgColor='cyan.200'>
+			<Flex justify='end'>
+				<ColorModeSwitcher />
+			</Flex>
+		</Box>
+	);
+};
 
 export default Navbar;
