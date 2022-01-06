2434c71119befcb491d98ba36a06d8b4ee6e10f5
EgimenoS <ergisan@gmail.com>
2022-01-06 20:21:22 +0100

    chore: changed schema of db bt separating playlist and video tables

--- src/models/db.js
+++ src/models/db.js
@@ -3,14 +3,20 @@ import Dexie from 'dexie';
 class PlayListsDB extends Dexie {
 	constructor() {
 		super('PlayListsDB');
-		this.version(1).stores({ playlists: '++id,title,videos' });
+		this.version(1).stores({ playlists: '++id,title', videos: '++id, title, handler, completed, playlistId' });
 	}
 
 	addPlaylist(playlist) {
-		return this.playlists.add({
-			title: playlist.title,
-			videos: playlist.videos,
-		});
+		return this.playlists
+			.add({
+				title: playlist.title,
+			})
+			.then((id) => {
+				for (const video of playlist.videos) {
+					this.videos.add({ title: video.name, handler: video, completed: false, playlistId: id });
+				}
+				return id;
+			});
 	}
 }
 
