2305353f585be9509ae7543e814f4b9aa5cd43ec
EgimenoS <ergisan@gmail.com>
2022-01-08 20:20:48 +0100

    feat: added video player

--- src/pages/PlayListDetail/components/Player.js
+++ src/pages/PlayListDetail/components/Player.js
@@ -1,7 +1,12 @@
-import { Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+import ReactPlayer from 'react-player';
 
 const Player = ({ url }) => {
-	return <Text>Playing {JSON.stringify(url)}</Text>;
+	return (
+		<Flex mb='4' align='center' direction='column'>
+			<ReactPlayer controls='true' url={url} />
+		</Flex>
+	);
 };
 
 export default Player;
