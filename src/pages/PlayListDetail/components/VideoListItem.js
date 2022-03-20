d98c188841abf9e155a5e874f66ada472f22afa2
EgimenoS <ergisan@gmail.com>
2022-03-20 19:35:16 +0100

    style: improved styles in videolosyitem

--- src/pages/PlayListDetail/components/VideoListItem.js
+++ src/pages/PlayListDetail/components/VideoListItem.js
@@ -1,4 +1,4 @@
-import { Text, Flex, IconButton, Tag, Icon } from '@chakra-ui/react';
+import { Text, Flex, IconButton, Tag, Icon, Badge } from '@chakra-ui/react';
 import { DeleteIcon } from '@chakra-ui/icons';
 import { BsPlayFill } from 'react-icons/bs';
 import humanizedDuration from '../../../utils/timeUtils';
@@ -17,19 +17,19 @@ const VideoListItem = ({ video, handlePlayVideo, handleDeleteVideo }) => {
 			borderColor='gray.200'
 		>
 			<Tag mr='4'>{video.position}</Tag>
-			<Text mr='4' fontSize='md' color='purple.700' fontWeight='bold'>
+			<Text isTruncated flexBasis='50%' justifySelf='start' mr='4' fontSize='md' color='purple.700' fontWeight='bold'>
 				{video.title}
 			</Text>
 			<Text fontSize='md' color='purple.700' fontWeight='bold'>
 				{humanizedDuration(video.duration)}
 			</Text>
 			{video.completed && (
-				<Text fontWeight='bold' color='purple.700' mr='4' ml='auto'>
-					COMPLETED!
-				</Text>
+				<Badge mr='4' ml='auto' variant='subtle' colorScheme='green'>
+					completed
+				</Badge>
 			)}
 			<IconButton
-				ml='auto'
+				ml={!video.completed ? 'auto' : ''}
 				mr='4'
 				boxShadow='rgb(14 14 44 / 40%) 0px -1px 0px 0px inset'
 				backgroundColor='accent'
